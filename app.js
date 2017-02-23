9f0eae9328a56f2ae76d58b7105f3c86e8b63ff3
Emmanuel Garcia <egarcia410@gmail.com>
2017-02-22 23:53:16 -0600

    Create: model schema

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
+var controller = require('./controllers/user.controller');
 
 var port = 8080;
 var db = 'mongodb://localhost:userExample';
@@ -14,6 +15,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+app.post('/', controller.register);
 
 app.listen(port, function(){
     console.log('Creating magic on ' + port);
